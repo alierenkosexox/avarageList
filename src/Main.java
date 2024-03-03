22a86ec682f1e9f63408492ce1dd19eec5623412
Ali Eren KÖSE <alierenkose@gmail.com>
2024-03-04 01:46:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+// Ali Eren KÖSE 04/03/2024 01:45
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0.0;
+        System.out.print("N sayısı giriniz: "); // N sayısı iste ve kaydet
+        int number = scanner.nextInt();
+        double[] list = new double[number];  // n sayısı ile liste oluştur
+        for (int i = 0; i < list.length; i++) {  // listenin harmonik serisini hesapla
+            list[i] = (double) 1 / (i + 1);
+            sum += list[i];
+        }
+        double average = list.length / sum;  //harmonik ortalama işlemini yap ve yazdır
+        System.out.println(average);
+    }
+}
